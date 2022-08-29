026ca1d3b4a05f28f872bbc1f7421e8e84af365a
EC2 Default User <ec2-user@ip-172-31-13-49.ap-northeast-1.compute.internal>
2022-08-29 08:03:21 +0000

    Chapter 8 バリデーションの設定

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ApplicationRecord
   has_one_attached :image
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true, blob: { content_type: :image }
+  # validates :image, attached_file_presence: true
 end
